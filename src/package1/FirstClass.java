b430de6a23a3084a062dbb2279aca05a295f00c7
Selcuk Cellik <sel2014@hotmail.com>
2022-12-13 22:16:57 +0000

    initial commit

---
+++ src/package1/FirstClass.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("Hello Worl");
+
+        }
+
+
+    }
+}
