ef8d3d059a825f05f160fde2864b4d8093186aad
HAVVA <havseymeldemirel@gmail.com>
2022-12-13 22:43:30 +0000

    havva

--- src/package1/Havva.java
+++ src/package1/Havva.java
@@ -2,6 +2,6 @@ package package1;
 
 public class Havva {
     public static void main(String[] args) {
-        System.out.println("havva demirel");
+        System.out.println("havva Dmirel");
     }
 }
